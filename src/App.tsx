422c43fc569907fdf0b9c26afb04187a9246e83c
waywardgod <ukwauchechukwu@gmail.com>
2022-01-25 16:37:34 +0100

    keyboard layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,22 @@ import {
   StyleSheet,
   Text,
   View,
+  Dimensions,
 } from 'react-native';
 import React from 'react';
 import Podium from './icons/Podium';
 import Settings from './icons/Settings';
 import Help from './icons/Help';
+import BackSpace from './icons/BackSpace';
+
+const {width} = Dimensions.get('window');
 
 const X = Array.from(Array(6).keys());
 const Y = Array.from(Array(5).keys());
+
+const keyFirstRow = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
+const keySecondRow = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
+const keyThirdRow = ['Z', 'X', 'C', 'V', 'B', 'N', 'M'];
 
 const App = () => {
   return (
@@ -50,6 +58,37 @@ const App = () => {
             </View>
           ))}
         </View>
+
+        <View style={styles.keyboard}>
+          <View style={styles.keyboardRow}>
+            {keyFirstRow.map(key => (
+              <Pressable key={key} style={styles.keyboardBtn}>
+                <Text style={styles.keyboardText}>{key}</Text>
+              </Pressable>
+            ))}
+          </View>
+          <View style={styles.keyboardRow}>
+            {keySecondRow.map(key => (
+              <Pressable key={key} style={styles.keyboardBtn}>
+                <Text style={styles.keyboardText}>{key}</Text>
+              </Pressable>
+            ))}
+          </View>
+
+          <View style={styles.keyboardRow}>
+            <Pressable style={[styles.keyboardBtn, styles.specialKey]}>
+              <Text style={styles.enterKey}>ENTER</Text>
+            </Pressable>
+            {keyThirdRow.map(key => (
+              <Pressable key={key} style={styles.keyboardBtn}>
+                <Text style={styles.keyboardText}>{key}</Text>
+              </Pressable>
+            ))}
+            <Pressable style={[styles.keyboardBtn, styles.specialKey]}>
+              <BackSpace color="#fff" width={20} height={20} />
+            </Pressable>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -61,6 +100,44 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#121213',
+  },
+  keyboardRow: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+  specialKey: {
+    flexGrow: 1,
+  },
+  enterKey: {
+    color: '#fff',
+    fontSize: 18,
+  },
+  keyboard: {
+    width: width,
+    maxWidth: 500,
+    paddingHorizontal: 4,
+  },
+  padding: {
+    height: 50,
+    flexGrow: 1,
+    marginHorizontal: 4,
+  },
+  keyboardBtn: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    marginHorizontal: 2,
+    minWidth: width / 10 - 5,
+    backgroundColor: '#3a3a3c',
+    height: 50,
+    borderWidth: 1,
+    borderColor: '#3a3a3c',
+  },
+  keyboardText: {
+    color: '#fff',
+    fontSize: 16,
   },
   grid: {
     margin: 10,
